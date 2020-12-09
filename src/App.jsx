8c0a2fb2eb02b135b02cd47d10d32821a918db0e
williamsimms <williamsimms193@gmail.com>
2020-12-08 19:51:56 -0500

    fixes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Box from './components/Box/Box'
+import './App.scss'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>app</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Box.scss'
+
+const Box = () => {
+  return <div className='box'></div>
+}
+
+export default Box
